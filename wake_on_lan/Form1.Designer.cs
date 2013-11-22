021adabcb7923edaab417967017c31da65a588c4
Жлобенцев Владимир <dnclive@gmail.com>
2013-11-22 12:02:37 +0400

    all done

---
+++ wake_on_lan/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace wake_on_lan
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cmb_mac = new System.Windows.Forms.ComboBox();
+			this.txt_log = new System.Windows.Forms.RichTextBox();
+			this.btn_wake = new System.Windows.Forms.Button();
+			this.txt_mac = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// cmb_mac
+			// 
+			this.cmb_mac.FormattingEnabled = true;
+			this.cmb_mac.Location = new System.Drawing.Point(12, 12);
+			this.cmb_mac.Name = "cmb_mac";
+			this.cmb_mac.Size = new System.Drawing.Size(298, 24);
+			this.cmb_mac.TabIndex = 0;
+			// 
+			// txt_log
+			// 
+			this.txt_log.Location = new System.Drawing.Point(12, 86);
+			this.txt_log.Name = "txt_log";
+			this.txt_log.Size = new System.Drawing.Size(676, 273);
+			this.txt_log.TabIndex = 1;
+			this.txt_log.Text = "";
+			// 
+			// btn_wake
+			// 
+			this.btn_wake.Location = new System.Drawing.Point(12, 42);
+			this.btn_wake.Name = "btn_wake";
+			this.btn_wake.Size = new System.Drawing.Size(75, 38);
+			this.btn_wake.TabIndex = 2;
+			this.btn_wake.Text = "Wake!";
+			this.btn_wake.UseVisualStyleBackColor = true;
+			this.btn_wake.Click += new System.EventHandler(this.btn_wake_Click);
+			// 
+			// txt_mac
+			// 
+			this.txt_mac.Location = new System.Drawing.Point(316, 12);
+			this.txt_mac.Name = "txt_mac";
+			this.txt_mac.Size = new System.Drawing.Size(372, 22);
+			this.txt_mac.TabIndex = 3;
+			this.txt_mac.Text = "1c-6f-65-56-f1-c9";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(700, 371);
+			this.Controls.Add(this.txt_mac);
+			this.Controls.Add(this.btn_wake);
+			this.Controls.Add(this.txt_log);
+			this.Controls.Add(this.cmb_mac);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox cmb_mac;
+		private System.Windows.Forms.RichTextBox txt_log;
+		private System.Windows.Forms.Button btn_wake;
+		private System.Windows.Forms.TextBox txt_mac;
+	}
+}
+
---
+++ wake_on_lan/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace wake_on_lan
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btn_wake_Click(object sender, EventArgs e)
+		{
+			using (UdpClient udpClient = new UdpClient())
+			{
+				Console.WriteLine("Enter mac address");
+				byte[] mac = StrToMac(txt_mac.Text);
+				udpClient.Send(mac, mac.Length, new IPEndPoint(IPAddress.Broadcast, 9));
+			}
+		}
+
+		static byte[] StrToMac(string s)
+		{
+			List<byte> arr = new List<byte>(102);
+
+			string[] macs = s.ToUpper().Split(' ', ':', '-');
+
+			for (int i = 0; i < 6; i++)
+				arr.Add(0xff);
+
+			for (int j = 0; j < 16; j++)
+				for (int i = 0; i < 6; i++)
+					arr.Add(Convert.ToByte(macs[i], 16));
+
+			return arr.ToArray();
+		}
+	}
+}
